17ffd8892495ea03df8addbe61b93c486f88415d
cyp <supercyp971@gmail.com>
2025-06-22 14:55:11 +0200

    feat: add prettyPrintJson for debugging

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 const mobileDesktopBreakpoint = 800;
 
 bool isDesktopLayout(BuildContext context) {
   return MediaQuery.of(context).size.width >= mobileDesktopBreakpoint;
+}
+
+void prettyPrintJson(Map<String, dynamic> json) {
+  JsonEncoder encoder = JsonEncoder.withIndent('  ');
+  String prettyprint = encoder.convert(json);
+  print(prettyprint);
 }
